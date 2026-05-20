c6ceaaf7d2362bd6002f1dfb9f3572589c039404
ismail essadik <prgaming.ismail@gmail.com>
2026-05-20 15:07:43 +0100

    env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,14 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- Tailwind CSS Play CDN (utility classes) --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- Custom component & shell CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- Alpine.js --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    {{-- Bootstrap JS (modals) --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
     @stack('styles')
